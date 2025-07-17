666e10edac1fd90c9494e7146a2730c2d998a038
Kazuhiro-Obama <38544837+PEPSINEX@users.noreply.github.com>
2025-07-17 23:32:04 +0900

    最小限のヘッダーを追加 (#6)
    * スタイル関連の構成を整理
    
    - styles フォルダを作成
    - パスエイリアス（@/styles）を追加
    - グローバルスタイルをエイリアスから読み込むよう変更
    
    * 最低限のヘッダーを作成
    
    - components フォルダを作成
    - パスエイリアス（@/components）を追加
    - 最低限のHeaderコンポーネントを作成
    - RootLayoutにてHeaderコンポーネントを呼び出す

---
+++ my-next-app/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="p-4 bg-gray-100 border-b border-gray-300">
+      <h1 className="text-neutral-900">行動記録アプリ</h1>
+    </header>
+  )
+}
