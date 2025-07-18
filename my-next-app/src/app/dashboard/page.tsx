2e7d2cbee8a0984d740faebc3a58590f25be0628
Kazuhiro-Obama <kagakufact@yahoo.co.jp>
2025-07-18 12:04:50 +0900

    認証データの保存・読み取りの仕組みを実装
    - userデータは data/users.json のJSONファイルに保存。保存するキーは後続タスクにて修正する
    - ファイル読み込み用のパスエイリアス @/lib/path を追加
    - 読み込み・書き込みのテストのため src/app/dashboard/page.tsx に最低限の記述を追加

--- my-next-app/src/app/dashboard/page.tsx
+++ my-next-app/src/app/dashboard/page.tsx
@@ -1,4 +1,13 @@
+import fs from "fs";
+import { USERS_JSON_PATH } from '@/lib/paths'
+
 export default function DashboardPage() {
+  const usersJson = fs.readFileSync(USERS_JSON_PATH, 'utf-8');
+  const users = JSON.parse(usersJson);
+
+  console.log(users);
+  users.push({ id: 101, name: 'new user' });
+
   return (
     <main className="p-4">
       <h1 className="text-2xl font-bold">ダッシュボード_changed</h1>
